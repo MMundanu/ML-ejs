c5bb82a82c70a8a4d8aa533988d9a4f9690ffb4a
MMundanu <mmundanu@yahoo.com>
2022-08-24 18:12:42 -0300

    tp finalizado

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -19,7 +19,12 @@ const controller = {
 		})
 	},
 	search: (req, res) => {
-		// Do the magic
+		const products = loadProducts();
+        const result = products.filter(product => product.name.toLowerCase().includes(req.query.keywords.toLowerCase()))
+        return res.render('products', {
+            products : result,
+            keywords : req.query.keywords
+        })
 	},
 };
 
